e5fb1c7eb37026d03917737a5e7de9f63f83a972
Mackenzie Malainey <macmalainey@gmail.com>
2022-03-02 13:47:54 -0700

    refactor part1

--- src/avl.rs
+++ src/avl.rs
@@ -4,68 +4,24 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use std::cmp::{Ordering, max};
 
-use super::tree::TreeDir;
+use super::tree::*;
 use super::tree::TreeDir::*;
 
-type AVLBranch<T> = Rc<RefCell<AVLNode<T>>>;
-type AVLTree<T> = Option<AVLBranch<T>>;
+pub type AVLBranch<T> = Rc<RefCell<AVLNode<T>>>;
+pub type AVLTree<T> = Option<AVLBranch<T>>;
 
-struct AVLNode<T: Ord + Debug> {
-    key: T,
-    height: usize,
-    left: AVLTree<T>,
-    right: AVLTree<T>,
-}
-
-enum TreeOpPath {
-    Insert(TreeDir, TreeDir),
-    Delete(TreeDir)
-}
-
-fn bst_rotate<T: Ord + Debug>(p: &AVLBranch<T>, direction: TreeDir) {
-    // Break apart the tree
-    let x = {
-        p.borrow_mut().prune(direction).unwrap()
-    };
-    let v = {
-        x.borrow_mut().prune(direction.reflect())
-    };
-
-    // Swap P and X
-    p.swap(&x);
-
-    // Add V to P
-    { *x.borrow_mut().get_child_mut(direction) = v };
-
-    // Add P to X
-    { *p.borrow_mut().get_child_mut(direction.reflect()) = Some(Rc::clone(&x)) };
-
-    // Update heights
-    { x.borrow_mut().update_height() };
-    { p.borrow_mut().update_height() };
-}
-
-fn avl_check<T: Ord + Debug>(parent: &AVLBranch<T>, uncle: &AVLTree<T>) -> bool {
-    let uheight = uncle.as_ref().map_or(0, |node| node.borrow().height);
-    let pheight = parent.borrow().get_height();
-
-    // If we properly insert every time this should never assert
-    assert!(pheight >= uheight);
-    pheight - uheight > 1
-}
-
-fn avl_check_beta<T: Ord + Debug>(parent: &AVLTree<T>, uncle: &AVLTree<T>) -> bool {
+pub fn avl_check<T: Ord>(parent: &AVLTree<T>, uncle: &AVLTree<T>) -> bool {
     let uheight = uncle.as_ref().map_or(0, |node| node.borrow().get_height());
     let pheight = parent.as_ref().map_or(0, |node| node.borrow().get_height());
 
     pheight - uheight > 1
 }
 
-fn avl_balance<T: Ord + Debug>(rnode: &AVLBranch<T>, op: TreeOpPath) {
+pub fn avl_balance<T: Ord>(rnode: &AVLBranch<T>, op: TreeOp) {
     match op {
-        TreeOpPath::Insert(ppath, xpath) => {
+        TreeOp::Insert(ppath, xpath) => {
             let rebalance = {
-                avl_check_beta(rnode.borrow().get_child(ppath), rnode.borrow().get_child(ppath.reflect()))
+                avl_check(rnode.borrow().get_child(ppath), rnode.borrow().get_child(ppath.reflect()))
             };
 
             if rebalance {
@@ -79,10 +35,10 @@ fn avl_balance<T: Ord + Debug>(rnode: &AVLBranch<T>, op: TreeOpPath) {
             }
         },
         // For deletion we treat the definitions as flipped (parent path becomes longer path)
-        TreeOpPath::Delete(upath) => {
+        TreeOp::Delete(upath) => {
             let ppath = upath.reflect();
             let rebalance = {
-                avl_check_beta(rnode.borrow().get_child(ppath), rnode.borrow().get_child(upath))
+                avl_check(rnode.borrow().get_child(ppath), rnode.borrow().get_child(upath))
             };
             if rebalance {
                 {
@@ -115,249 +71,16 @@ fn avl_balance<T: Ord + Debug>(rnode: &AVLBranch<T>, op: TreeOpPath) {
     }
 }
 
-fn bst_pop<T: Ord + Debug>(tree: &mut AVLTree<T>) -> Result<T, ()> {
-    let successor = {
-        let mut x = tree.as_ref().unwrap().borrow_mut();
-        if x.get_child(Right).is_some() && x.get_child(Left).is_some() {
-            return Err(())
-        } else {
-            x.prune(Right)
-                .or_else(|| x.prune(Left))
-        }
-    };
-
-    let popped = std::mem::replace(tree, successor);
-    Ok(Rc::try_unwrap(popped.unwrap()).unwrap().into_inner().pop())
+pub struct AVLNode<T: Ord> {
+    key: T,
+    height: usize,
+    left: AVLTree<T>,
+    right: AVLTree<T>,
 }
 
-fn bst_delete<T: Ord + Debug>(root: &mut AVLTree<T>, key: &T) -> Option<T> {
+impl <T: Ord> AVLNode<T> {
 
-    let (mut node_stack, mut tree_nav) = if let Some(root_node) = root {
-        (
-            Vec::with_capacity(root_node.borrow().get_height()),
-            root_node.borrow().search(|nkey| key.cmp(nkey)).map(
-                |path| {
-                    (path, Rc::clone(root_node))
-                }
-            )
-        )
-    } else { 
-        return None
-    };
-
-    // Find the node we want to remove
-    tree_nav = if let Some((mut xpath, mut pnode)) = tree_nav {
-        loop {
-            let (path, node) = {
-                let p = pnode.borrow();
-                let xtree = p.get_child(xpath);
-
-                if let Some(xnode) = xtree {
-                    if let Some(path) = xnode
-                        .borrow()
-                        .search(|nkey| key.cmp(nkey)) {
-                            (path, Rc::clone(&xnode))
-                    } else {
-                        break; 
-                    }
-                } else {
-                    return None;
-                }
-            };
-
-            node_stack.push((xpath, Rc::clone(&pnode)));
-            xpath = path;
-            pnode = node;
-        }
-
-        Some((xpath, pnode))
-    } else {
-        tree_nav
-    };
-
-    let (owned_key, pnode) = {
-        let pnode;
-        let mut p;
-        let (xtree, pnode_clone) = if let Some((path, node)) = tree_nav {
-            pnode = node;
-            let clone = Rc::clone(&pnode);
-            p = pnode.borrow_mut();
-            (p.get_child_mut(path), Some(clone))
-        } else {
-            (root, None)
-        };
-
-        // We found the node to delete, now we check if we need to perform a swap
-        if let Ok(key) = bst_pop(xtree) {
-            // No swap needed
-            (key, pnode_clone.unwrap())
-        } else {
-            // We need to swap with the bottom-left most node
-            let mut xpath = Right;
-            let next_path = Left;
-
-            let mut pnode = Rc::clone(xtree.as_ref().unwrap());
-            {
-                let mut swap_node = Rc::clone(pnode.borrow().get_child(xpath).as_ref().unwrap());
-                loop {
-                    let node = {
-                        let s = swap_node.borrow();
-                        let next_tree = s.get_child(
-                            next_path
-                        );
-            
-                        if let Some(next_node) = next_tree {
-                            Rc::clone(next_node)
-                        } else {
-                            break;
-                        }
-                    };
-    
-                    node_stack.push((xpath, pnode));
-                    pnode = swap_node;
-                    swap_node = node;
-    
-                    xpath = Left;
-                }
-            }
-
-            // Get key and perform swap
-            let swap_key =
-                bst_pop(
-                    pnode.borrow_mut()
-                        .get_child_mut(xpath)
-                ).unwrap();
-
-            (xtree.as_ref().unwrap().borrow_mut().replace_key(swap_key), pnode)
-        }
-    };
-
-    pnode.borrow_mut().update_height();
-
-    while let Some((ppath, node)) = node_stack.pop() {
-        { node.borrow_mut().update_height(); }
-
-        avl_balance(&node, TreeOpPath::Delete(ppath));
-    }
-
-    Some(owned_key)
-}
-
-fn bst_insert<T: Ord + Debug>(root: &mut AVLTree<T>, key: T) {
-    let (mut node_stack, (mut xpath, mut pnode)) = if let Some(root_node) = root {
-        (
-            Vec::with_capacity(root_node.borrow().get_height()),
-            if let Some(path) = root_node.borrow().search(|nkey| key.cmp(nkey)) {
-                (path, Rc::clone(root_node))
-            } else {
-                return
-            }
-        )
-    } else { 
-        *root = Some(
-            Rc::new(
-                RefCell::new(
-                    AVLNode::new_with(key)
-                )
-            )
-        );
-        return
-    };
-
-    // Find where we want to insert
-    loop {
-        let (path, node) = {
-            let mut p = pnode.borrow_mut();
-            let xtree = p.get_child_mut(xpath);
-
-            if let Some(xnode) = xtree {
-                if let Some(path) = xnode
-                    .borrow()
-                    .search(|nkey| key.cmp(nkey)) {
-                        (path, Rc::clone(&xnode))
-                } else {
-                    return;
-                }
-            } else {
-                *xtree = Some(
-                    Rc::new(
-                        RefCell::new(
-                            AVLNode::new_with(key)
-                        )
-                    )
-                );
-                p.update_height();
-                break;
-            }
-        };
-
-        node_stack.push((xpath, Rc::clone(&pnode)));
-        xpath = path;
-        pnode = node;
-    }
-
-    while let Some((ppath, node)) = node_stack.pop() {
-        { node.borrow_mut().update_height(); }
-
-        avl_balance(&node, TreeOpPath::Insert(ppath, xpath));
-
-        xpath = ppath;
-    }
-}
-
-fn bst_insert_rec<T: Ord + Debug>(rnode: &AVLBranch<T>, key: T) -> Option<TreeDir> {
-    // If we didn't find the key grab the next child
-    // otherwise we return
-    let (parent_path, parent, uncle) = {
-        let r = rnode.borrow();
-        let next = r.search(|nkey| key.cmp(nkey));
-
-        if let Some(path) = next {
-            (
-                path,
-                // To prevent Rust from getting upset we temporarily get ownership the data here
-                r.get_child(path).as_ref().map(|parent| Rc::clone(parent)),
-                r.get_child(path.reflect()).as_ref().map(|uncle| Rc::clone(uncle)),  
-            )
-        } else {
-            return None;
-        }
-    };
-
-    // Further search the tree if possible otherwise we insert
-    if let Some(pnode) = parent {
-        match bst_insert_rec(&pnode, key) {
-            Some(child_path) => {
-                // Keep this in it's own scope to prevent a panic
-                { pnode.borrow_mut().update_height(); }
-
-                if avl_check(&pnode, &uncle) {
-                    if child_path != parent_path {
-                        bst_rotate(&pnode, child_path);
-                    }
-                    bst_rotate(rnode, parent_path)
-                }
-            }
-            None => return None
-        };
-    } else {
-        let mut r = rnode.borrow_mut();
-        *r.get_child_mut(parent_path) = Some(
-            Rc::new(
-                RefCell::new(
-                    AVLNode::new_with(key)
-                )
-            )
-        );
-        r.update_height();
-    }
-
-    Some(parent_path)
-}
-
-impl <T: Ord + Debug> AVLNode<T> {
-
-    fn new_with(key: T) -> Self {
+    pub fn new_with(key: T) -> Self {
         AVLNode {
             key,
             height: 1,
@@ -366,52 +89,51 @@ impl <T: Ord + Debug> AVLNode<T> {
         }
     }
 
-    fn update_height(&mut self) {
+    pub fn update_height(&mut self) {
         self.height = max(
             self.left.as_ref().map_or(0, |node| node.borrow().height),
             self.right.as_ref().map_or(0, |node| node.borrow().height)
         ) + 1;
     }
 
-    fn get_child(&self, pos: TreeDir) -> &AVLTree<T> {
+    pub fn get_child(&self, pos: TreeDir) -> &AVLTree<T> {
         match pos {
             Left => &self.left,
             Right => &self.right
         }
     }
 
-    fn get_child_mut(&mut self, pos: TreeDir) -> &mut AVLTree<T> {
+    pub fn get_child_mut(&mut self, pos: TreeDir) -> &mut AVLTree<T> {
         match pos {
             Left => &mut self.left,
             Right => &mut self.right
         }
     }
 
-    fn prune(&mut self, pos: TreeDir) -> AVLTree<T> {
+    pub fn prune(&mut self, pos: TreeDir) -> AVLTree<T> {
         match pos {
             Left => self.left.take(),
             Right => self.right.take()
         }
     }
 
-    fn search<F>(&self, compare: F) -> Option<TreeDir> 
-        where F: FnOnce(&T) -> Ordering {
-        match compare(&self.key) {
+    pub fn search(&self, key: &T) -> Option<TreeDir> {
+        match key.cmp(&self.key) {
             Ordering::Equal => None,
             Ordering::Greater => Some(Right),
             Ordering::Less => Some(Left)
         }
     }
 
-    fn get_height(&self) -> usize {
+    pub fn get_height(&self) -> usize {
         self.height
     }
 
-    fn pop(self) -> T {
+    pub fn pop(self) -> T {
         self.key
     }
 
-    fn replace_key(&mut self, mut key: T) -> T {
+    pub fn replace_key(&mut self, mut key: T) -> T {
         if let Some(ref node) = self.left {
             let n = node.borrow();
             assert_ne!(key.cmp(&n.key), Ordering::Less);
@@ -429,65 +151,7 @@ impl <T: Ord + Debug> AVLNode<T> {
 
 }
 
-pub struct Tree<T: Ord + Debug>(AVLTree<T>);
-
-impl <T: Ord + Debug> Tree<T> {
-    pub fn new() -> Self {
-        Tree(None)
-    }
-
-    pub fn insert_rec(&mut self, key: T) {
-        if let Some(ref branch) = self.0 {
-            bst_insert_rec(branch, key);
-        } else {
-            self.0 = Some(
-                Rc::new(
-                    RefCell::new(
-                        AVLNode::new_with(key)
-                    )
-                )
-            );
-        }
-    }
-
-    pub fn insert(&mut self, key: T) {
-        bst_insert(&mut self.0, key)
-    }
-
-    pub fn delete(&mut self, key: &T) -> Option<T> {
-        bst_delete(&mut self.0, key)
-    }
-
-    pub fn height(&self) -> usize {
-        self.0.as_ref().map_or(0, |node| node.borrow().height)
-    }
-}
-
-impl <T: Ord + Display + Debug> Display for Tree<T> {
-
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        match &self.0 {
-            Some(node) => {
-                write!(f, "Tree: {{ {}}}", node.borrow())
-            }
-            None => Ok(())
-        }
-    }
-}
-
-impl <T: Ord + Debug> Debug for Tree<T> {
-
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        match &self.0 {
-            Some(node) => {
-                node.borrow().fmt(f)
-            }
-            None => Ok(())
-        }
-    }
-}
-
-impl <T: Ord + Display + Debug> Display for AVLNode<T> {
+impl <T: Ord + Display> Display for AVLNode<T> {
 
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         match self.left {
--- src/tree.rs
+++ src/tree.rs
@@ -1,4 +1,8 @@
-use std::cmp::Ordering;
+use std::fmt::Display;
+use std::fmt::Debug;
+use std::cell::RefCell;
+use std::rc::Rc;
+use super::avl::*;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum TreeDir {
@@ -8,7 +12,7 @@ pub enum TreeDir {
 
 use TreeDir::*;
 impl TreeDir {
-
+    
     pub fn reflect(&self) -> TreeDir {
         match self {
             Left => Right,
@@ -18,19 +22,287 @@ impl TreeDir {
 
 }
 
-pub trait TreeNode<T: Ord> 
-    where Self: std::marker::Sized {
-
-    fn new_from_key(key: T) -> Self;
-    fn is_empty(key: T) -> bool;
-    fn pop(&mut self) -> T;
-    fn prune(&mut self, pos: TreeDir) -> Option<Self>;
-    fn get_child(&self, pos: TreeDir) -> Option<&Self>;
-    fn get_child_mut(&mut self, pos: TreeDir) -> Option<&mut Self>;
-    fn insert(&mut self, pos: TreeDir);
-    fn cmp(&self, key: &T) -> Ordering;
+pub enum TreeOp {
+    Insert(TreeDir, TreeDir),
+    Delete(TreeDir)
 }
 
-pub trait BalancedTreeNode<T: Ord>: TreeNode<T> {
+pub trait TreeNode<T: Ord> {
+    fn new_with(key: T) -> Self;
+
+    fn update_height(&mut self);
+    fn get_height(&self) -> usize;
+
+    fn get_child(&self, pos: TreeDir) -> &AVLTree<T>;
+    fn get_child_mut(&mut self, pos: TreeDir) -> &mut AVLTree<T>;
+    fn prune(&mut self, pos: TreeDir) -> AVLTree<T>;
+
+    fn search(&self, key: &T) -> Option<TreeDir>;
+
+    fn pop(self) -> T;
     
+    fn insert(&mut self, pos: TreeDir);
+    fn replace_key(&mut self, key: T) -> T;
 }
+
+impl <T: Ord> Tree<T> {
+    pub fn new() -> Self {
+        Tree(None)
+    }
+
+    pub fn insert(&mut self, key: T) {
+        bst_insert(&mut self.0, key)
+    }
+
+    pub fn delete(&mut self, key: &T) -> Option<T> {
+        bst_delete(&mut self.0, key)
+    }
+
+    pub fn height(&self) -> usize {
+        self.0.as_ref().map_or(0, |node| node.borrow().get_height())
+    }
+}
+
+impl <T: Ord + Display> Display for Tree<T> {
+
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        match &self.0 {
+            Some(node) => {
+                write!(f, "Tree: {{ {}}}", node.borrow())
+            }
+            None => Ok(())
+        }
+    }
+}
+
+impl <T: Ord + Debug> Debug for Tree<T> {
+
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        match &self.0 {
+            Some(node) => {
+                node.borrow().fmt(f)
+            }
+            None => Ok(())
+        }
+    }
+}
+
+pub fn bst_rotate<T: Ord>(p: &AVLBranch<T>, direction: TreeDir) {
+    // Break apart the tree
+    let x = {
+        p.borrow_mut().prune(direction).unwrap()
+    };
+    let v = {
+        x.borrow_mut().prune(direction.reflect())
+    };
+
+    // Swap P and X
+    p.swap(&x);
+
+    // Add V to P
+    { *x.borrow_mut().get_child_mut(direction) = v };
+
+    // Add P to X
+    { *p.borrow_mut().get_child_mut(direction.reflect()) = Some(Rc::clone(&x)) };
+
+    // Update heights
+    { x.borrow_mut().update_height() };
+    { p.borrow_mut().update_height() };
+}
+
+pub fn bst_pop<T: Ord>(tree: &mut AVLTree<T>) -> Result<T, ()> {
+    let successor = {
+        let mut x = tree.as_ref().unwrap().borrow_mut();
+        if x.get_child(Right).is_some() && x.get_child(Left).is_some() {
+            return Err(())
+        } else {
+            x.prune(Right)
+                .or_else(|| x.prune(Left))
+        }
+    };
+
+    let popped = std::mem::replace(tree, successor);
+    Ok(
+        Rc::try_unwrap(
+            popped.unwrap()
+        ).map_err(|_| ()) // map_err() call to satisfy the Debug trait bound
+        .unwrap().into_inner().pop()
+    )
+}
+
+pub fn bst_delete<T: Ord>(root: &mut AVLTree<T>, key: &T) -> Option<T> {
+
+    let (mut node_stack, mut tree_nav) = if let Some(root_node) = root {
+        (
+            Vec::with_capacity(root_node.borrow().get_height()),
+            root_node.borrow().search(&key).map(
+                |path| {
+                    (path, Rc::clone(root_node))
+                }
+            )
+        )
+    } else { 
+        return None
+    };
+
+    // Find the node we want to remove
+    tree_nav = if let Some((mut xpath, mut pnode)) = tree_nav {
+        loop {
+            let (path, node) = {
+                let p = pnode.borrow();
+                let xtree = p.get_child(xpath);
+
+                if let Some(xnode) = xtree {
+                    if let Some(path) = xnode
+                        .borrow()
+                        .search(&key) {
+                            (path, Rc::clone(&xnode))
+                    } else {
+                        break; 
+                    }
+                } else {
+                    return None;
+                }
+            };
+
+            node_stack.push((xpath, Rc::clone(&pnode)));
+            xpath = path;
+            pnode = node;
+        }
+
+        Some((xpath, pnode))
+    } else {
+        tree_nav
+    };
+
+    let (owned_key, pnode) = {
+        let pnode;
+        let mut p;
+        let (xtree, pnode_clone) = if let Some((path, node)) = tree_nav {
+            pnode = node;
+            let clone = Rc::clone(&pnode);
+            p = pnode.borrow_mut();
+            (p.get_child_mut(path), Some(clone))
+        } else {
+            (root, None)
+        };
+
+        // We found the node to delete, now we check if we need to perform a swap
+        if let Ok(key) = bst_pop(xtree) {
+            // No swap needed
+            (key, pnode_clone.unwrap())
+        } else {
+            // We need to swap with the bottom-left most node
+            let mut xpath = Right;
+            let next_path = Left;
+
+            let mut pnode = Rc::clone(xtree.as_ref().unwrap());
+            {
+                let mut swap_node = Rc::clone(pnode.borrow().get_child(xpath).as_ref().unwrap());
+                loop {
+                    let node = {
+                        let s = swap_node.borrow();
+                        let next_tree = s.get_child(
+                            next_path
+                        );
+            
+                        if let Some(next_node) = next_tree {
+                            Rc::clone(next_node)
+                        } else {
+                            break;
+                        }
+                    };
+    
+                    node_stack.push((xpath, pnode));
+                    pnode = swap_node;
+                    swap_node = node;
+    
+                    xpath = Left;
+                }
+            }
+
+            // Get key and perform swap
+            let swap_key =
+                bst_pop(
+                    pnode.borrow_mut()
+                        .get_child_mut(xpath)
+                ).unwrap();
+
+            (xtree.as_ref().unwrap().borrow_mut().replace_key(swap_key), pnode)
+        }
+    };
+
+    pnode.borrow_mut().update_height();
+
+    while let Some((ppath, node)) = node_stack.pop() {
+        { node.borrow_mut().update_height(); }
+
+        avl_balance(&node, TreeOp::Delete(ppath));
+    }
+
+    Some(owned_key)
+}
+
+pub fn bst_insert<T: Ord>(root: &mut AVLTree<T>, key: T) {
+    let (mut node_stack, (mut xpath, mut pnode)) = if let Some(root_node) = root {
+        (
+            Vec::with_capacity(root_node.borrow().get_height()),
+            if let Some(path) = root_node.borrow().search(&key) {
+                (path, Rc::clone(root_node))
+            } else {
+                return
+            }
+        )
+    } else { 
+        *root = Some(
+            Rc::new(
+                RefCell::new(
+                    AVLNode::new_with(key)
+                )
+            )
+        );
+        return
+    };
+
+    // Find where we want to insert
+    loop {
+        let (path, node) = {
+            let mut p = pnode.borrow_mut();
+            let xtree = p.get_child_mut(xpath);
+
+            if let Some(xnode) = xtree {
+                if let Some(path) = xnode
+                    .borrow()
+                    .search(&key) {
+                        (path, Rc::clone(&xnode))
+                } else {
+                    return;
+                }
+            } else {
+                *xtree = Some(
+                    Rc::new(
+                        RefCell::new(
+                            AVLNode::new_with(key)
+                        )
+                    )
+                );
+                p.update_height();
+                break;
+            }
+        };
+
+        node_stack.push((xpath, Rc::clone(&pnode)));
+        xpath = path;
+        pnode = node;
+    }
+
+    while let Some((ppath, node)) = node_stack.pop() {
+        { node.borrow_mut().update_height(); }
+
+        avl_balance(&node, TreeOp::Insert(ppath, xpath));
+
+        xpath = ppath;
+    }
+}
+
+pub struct Tree<T: Ord>(AVLTree<T>);
